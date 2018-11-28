e2775cb8820d3bb016a1fbdc868da0d824748ca5
wuly <596707410@qq.com>
2018-11-28 21:21:22 +0800

    123

--- maven_dao/src/main/java/com/example/demo1.java
+++ maven_dao/src/main/java/com/example/demo1.java
@@ -3,6 +3,5 @@ package com.example;
 
 public class demo1 {
 	public static void main(String[] args) {
-		System.out.println("qweqwe");
 	}
 }
---
+++ maven_web/src/main/java/maven_web/Good.java
@@ -0,0 +1,5 @@
+package maven_web;
+
+public class Good {
+
+}
